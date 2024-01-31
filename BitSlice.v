93c516992da292c119e5aeb5d918fb8ef5b131a7
jsdus <146771029+jsdus@users.noreply.github.com>
2024-01-31 11:43:12 -0500

    Update BitSlice.v

--- BitSlice.v
+++ BitSlice.v
@@ -6,7 +6,7 @@ output Y, Cout;
 
 wire logic,arith;
 
-LogicBlk1 inst0 (.a(A), .b(B), .s0(K0), .s1(K1), .y(logic));
+LogicBlk inst0 (.a(A), .b(B), .s0(K0), .s1(K1), .y(logic));
 ArithBlk inst1 (.S1(K1), .S0(K0), .A(A), .B(B), .C(Cin), .Y(arith), .CO(Cout));
 MUX_21 M42 (.in0(logic), .in1(arith), .s(K2), .y(Y));
 endmodule
